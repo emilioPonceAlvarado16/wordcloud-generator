71278af3e0a43e293794cb5e490b10a83ba1692c
Isaias Ponce <isaiasponce-12@hotmail.es>
2022-02-03 22:18:13 -0500

    new changes

--- components/app/App.js
+++ components/app/App.js
@@ -1,14 +1,18 @@
-import { React, useState, useEffect } from 'react';
+import { React, useState, useEffect, createRef } from 'react';
 import ReactWordcloud from 'react-wordcloud';
+import { saveSvgAsPng } from 'save-svg-as-png';
+import Textdata from '../textdata/Textdata';
+
 // import getFrequencies from "../../functions/getFrequencies"
 // import Data from "../../data/Data"
 export default function App() {
-  const data=["the", "a", "to", "if", "is", "in","it", "of", "and", "or", "an", "as", "i", "me", "my","we", "our", "ours", "you", "your", "yours", "he", "she", "him", "his", "her", "hers", "its", "they", "them",
-  "their", "what", "which", "who", "whom", "this", "that", "am", "are", "was", "were", "be", "been", "being",
-  "have", "has", "had", "do", "does", "did", "but", "at", "by", "with", "from", "here", "when", "where", "how",
-  "all", "any", "both", "each", "few", "more", "some", "such", "no", "nor", "too", "very", "can", "will", "just"];
+  const wordcloudRef = createRef();
+  const data = ["the", "a", "to", "if", "is", "in", "it", "of", "and", "or", "an", "as", "i", "me", "my", "we", "our", "ours", "you", "your", "yours", "he", "she", "him", "his", "her", "hers", "its", "they", "them",
+    "their", "what", "which", "who", "whom", "this", "that", "am", "are", "was", "were", "be", "been", "being",
+    "have", "has", "had", "do", "does", "did", "but", "at", "by", "with", "from", "here", "when", "where", "how",
+    "all", "any", "both", "each", "few", "more", "some", "such", "no", "nor", "too", "very", "can", "will", "just"];
   //  function feq(value){ getFrequencies(value)};
-  
+
   const initialVal = `What is Lorem Ipsum?
   Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
   
@@ -18,95 +22,102 @@ export default function App() {
   
   Where does it come from?
   `
-  
-  
-  const [state, setState] = useState(initialVal);
-  
+
+
+  const [textdata, setTextdata] = useState(initialVal);
+
   const [worcloud, setworcloud] = useState({});
 
-useEffect(() => {
-  const wl = {
-    size: [400, 300],
-    options: {
-      rotations: 2,
-      rotationAngles: [-90, 0],
-      fontSizes: [1000, 1000],
-      fontFamily: 'courier new',
-    },
-    
-    started: false,
-    words: [
-      {
-        text: 'told',
-        value: 7,
+  useEffect(() => {
+    const wl = {
+      size: [60, 70],
+      options: {
+        rotations: 2,
+        rotationAngles: [-90, 0],
+        fontSizes: [1000, 1000],
+        fontFamily: 'courier new',
       },
-      {
-        text: 'mistake',
-        value: 1,
-      },
-      {
-        text: 'thought',
-        value: 2,
-      },
-      {
-        text: 'bad',
-        value: 4,
-      },
-    ]
-  };
 
-  return () => {
-   setworcloud({started:true, ...worcloud})
-  };
-}, []);
+      started: false,
+      words: [
+        {
+          text: 'told',
+          value: 7,
+        },
+        {
+          text: 'mistake',
+          value: 1,
+        },
+        {
+          text: 'thought',
+          value: 2,
+        },
+        {
+          text: 'bad',
+          value: 4,
+        },
+      ]
+    };
 
-  
+    return () => {
+      setworcloud({ started: true, ...worcloud })
+    };
+  }, []);
+
+
 
 
   const update = (e) => {
 
     const new_value = e.target.value;
-    setState(new_value);
+    setTextdata(new_value);
     // const wd = () => getFrequencies(new_value);
     console.log();
     // setworcloud({ started:true,...worcloud });
 
   }
-const getFrequencies = (text = '') => {
+  const getFrequencies = (text = '') => {
     var list = text.split(" ");
-    if(text.trim(" ")=="")return;
+    if (text.trim(" ") == "") return;
     var result = [];
     var word_list = [];
-    var index=-1;
+    var index = -1;
+    var word = ""
     for (var i = 0; i < list.length; i++) {
-        if (!data.includes(list[i])) {
-            if (!word_list.includes(list[i])) {
-                word_list.push(list[i]);
-                result.push({ text: list[i], value: 1 })
-            }
-            else {
-                    index=result.findIndex((obj)=>{
-                        return obj.text==list[i];
-                    });
-                    result[index].value=result[index].value+1;
-
-            }
+      word = list[i].toLocaleLowerCase();
+      if (!data.includes(word)) {
+        if (!word_list.includes(list[i])) {
+          word_list.push(list[i]);
+          result.push({ text: list[i], value: 1 })
         }
+        else {
+          index = result.findIndex((obj) => {
+            return obj.text == list[i];
+          });
+          result[index].value = result[index].value + 1;
+
+        }
+      }
     }
     return result;
 
-}
+  }
+
   const processData = () => {
 
     // var wd="";
-   
-   const wd=getFrequencies(state);
-  //  setTimeout(console.log())
-   
-    console.log(wd);
-    setworcloud({ ...worcloud ,started:true ,words: wd});
 
-  }
+    const wd = getFrequencies(textdata);
+    //  setTimeout(console.log())
+
+    console.log(wd);
+    setworcloud({ ...worcloud, started: true, words: wd });
+
+  };
+  const handleSave = () => {
+    const svgElement = wordcloudRef.current.querySelector('svg');
+    saveSvgAsPng(svgElement, 'wordcloud.png');
+  };
 
   return (
     <>
@@ -115,39 +126,39 @@ const getFrequencies = (text = '') => {
           <div className="container text-center">
             <h2>Free Wordcloud Generator</h2>
             <p className="lead">Convert text into a wordcloud image in just a few clicks.</p>
-            <button className="btn social-icons" ><i className="fa fa-upload" aria-hidden="true"> Upload File .txt</i></button> <button className="btn social-icons" ><i className="fa fa-download" aria-hidden="true"> Download Image</i></button> <button className="btn btn-success" onClick={processData}><i className="fa fa-play" aria-hidden="true"></i> Run</button>
+
+            <button className="btn social-icons " ><i className="fa fa-upload" aria-hidden="true"> Upload File .txt</i></button>
+            <button className="btn social-icons" onClick={handleSave} ><i className="fa fa-download" aria-hidden="true"> Download Image</i></button>
+            <button className="btn btn-success" onClick={processData}><i className="fa fa-play" aria-hidden="true"></i> Run</button>
+
           </div>
         </div>
       </header>
       <div className="row  justify-content-center  mt-5  " >
 
-        <textarea id="text1" name="text1" type="text"
-
-          style={{ width: '100%', height: 'calc( 40em + 15vh )', fontSize: '18px' }}
-          className="col-5 col-sm-5 col-md-5 col-lg-5  "
-          placeholder={state}
-          autoComplete="false"
-          onChange={update}
-        >
-          {state}
-
-        </textarea>
+        <Textdata textdata={textdata} update={update}></Textdata>
 
 
-        <div id="text1" name="text1" type="text"
+        <div name="text1" type="text"
 
-          style={{ border: '1px solid #333', width: '100%', height: 'calc( 40em + 15vh )',  border: '1px solid #333', overflow: 'auto'}}
+          style={{ border: '1px solid #333', width: '100%', height: 'calc( 40em + 23.5vh )', border: '1px solid #333', overflow: 'auto' }}
 
-          className="bg-white col-5 col-sm-5 col-md-5 col-lg-5  " >
+          className="bg-dark col-5 col-sm-5 col-md-5 col-lg-5  " >
+          <div style={{ width: "28%", height: "30%" }} className=" container d-flex justify-content-center">
+            {worcloud.started &&
+              <span ref={wordcloudRef}>
+                <ReactWordcloud
+                  words={worcloud.words}
 
-          {worcloud.started && <ReactWordcloud
-            words={worcloud.words}
-            size={worcloud.size}
-            options={worcloud.options}
-            
-            
-            
-          />}
+                  options={worcloud.options} /></span>}
+
+
+          </div>
+
+
+
+
+
         </div>
       </div>
 
---
+++ components/textdata/Textdata.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function Textdata(props) {
+    const {textdata, update}=props;
+  return (
+
+    <textarea id="text1" name="text1" type="text"
+
+          style={{ width: '100%', height: 'calc( 40em + 15vh )', fontSize: '18px' }}
+          className="col-5 col-sm-5 col-md-5 col-lg-5  "
+          placeholder="Copy text here and then Run"
+          autoComplete="false"
+          onChange={update}
+        >
+          {textdata}
+
+        </textarea>
+
+  )
+}
